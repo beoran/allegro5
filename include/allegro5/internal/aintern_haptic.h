dd983525348a69fbe7b4e17539b77b19fb0a1e52
Beoran <beoran@gmail.com>
2013-06-21 15:09:36 +0200

    Interface and implementation of haptic devices with linux driver and an example.

--- include/allegro5/internal/aintern_haptic.h
+++ include/allegro5/internal/aintern_haptic.h
@@ -19,25 +19,33 @@ typedef struct ALLEGRO_HAPTIC_DRIVER
    const char * hapdrv_ascii_name;
    AL_METHOD(bool, init_haptic, (void));
    AL_METHOD(void, exit_haptic, (void));
-   AL_METHOD(bool, reconfigure_haptics, (void));
-   AL_METHOD(int, num_haptics, (void));
-   AL_METHOD(ALLEGRO_HAPTIC *, get_haptic, (int));
-   AL_METHOD(void, release_haptic    , (ALLEGRO_HAPTIC *));
-   AL_METHOD(int, get_haptic_flags   , (ALLEGRO_HAPTIC *));
-   AL_METHOD(const char *, get_name  , (ALLEGRO_HAPTIC *));
-   AL_METHOD(bool, get_active        , (ALLEGRO_HAPTIC *));
+   
    AL_METHOD(bool, is_mouse_haptic   , (ALLEGRO_MOUSE *));
    AL_METHOD(bool, is_joystick_haptic, (ALLEGRO_JOYSTICK *));
+   AL_METHOD(bool, is_keyboard_haptic, (ALLEGRO_KEYBOARD *));
+   AL_METHOD(bool, is_display_haptic , (ALLEGRO_DISPLAY *));
+   AL_METHOD(bool, is_touch_input_haptic , (ALLEGRO_TOUCH_INPUT *));
+   
    AL_METHOD(ALLEGRO_HAPTIC *, get_from_mouse   , (ALLEGRO_MOUSE *));
    AL_METHOD(ALLEGRO_HAPTIC *, get_from_joystick, (ALLEGRO_JOYSTICK *));
-   AL_METHOD(int , get_num_axes      , (ALLEGRO_HAPTIC *)); 
+   AL_METHOD(ALLEGRO_HAPTIC *, get_from_keyboard, (ALLEGRO_KEYBOARD *));
+   AL_METHOD(ALLEGRO_HAPTIC *, get_from_display , (ALLEGRO_DISPLAY *));
+   AL_METHOD(ALLEGRO_HAPTIC *, get_from_touch_input, (ALLEGRO_TOUCH_INPUT *));
+
+   AL_METHOD(bool  , get_active        , (ALLEGRO_HAPTIC *)); 
+   AL_METHOD(int   , get_capabilities  , (ALLEGRO_HAPTIC *));   
+   AL_METHOD(double, get_gain          , (ALLEGRO_HAPTIC *));
+   AL_METHOD(bool  , set_gain          , (ALLEGRO_HAPTIC *, double));
+   AL_METHOD(int   , get_num_effects   , (ALLEGRO_HAPTIC *));   
+   
    AL_METHOD(bool, is_effect_ok      , (ALLEGRO_HAPTIC *, ALLEGRO_HAPTIC_EFFECT *));
-   AL_METHOD(bool, upload_effect     , (ALLEGRO_HAPTIC *, ALLEGRO_HAPTIC_EFFECT *, int *));
-   AL_METHOD(bool, play_effect       , (ALLEGRO_HAPTIC *, int, int ));
-   AL_METHOD(bool, stop_effect       , (ALLEGRO_HAPTIC *, int));
-   AL_METHOD(bool, is_effect_stopped , (ALLEGRO_HAPTIC *, int));
-   AL_METHOD(bool, stop_all_efects   , (ALLEGRO_HAPTIC *, int));
-   AL_METHOD(bool, is_effect_playing , (ALLEGRO_HAPTIC *, int));
+   AL_METHOD(bool, upload_effect     , (ALLEGRO_HAPTIC *, ALLEGRO_HAPTIC_EFFECT *, ALLEGRO_HAPTIC_EFFECT_ID *));
+   AL_METHOD(bool, play_effect       , (ALLEGRO_HAPTIC_EFFECT_ID *, int));
+   AL_METHOD(bool, stop_effect       , (ALLEGRO_HAPTIC_EFFECT_ID *));   
+   AL_METHOD(bool, is_effect_playing , (ALLEGRO_HAPTIC_EFFECT_ID *));   
+   AL_METHOD(bool, stop_all_efects   , (ALLEGRO_HAPTIC *));
+   AL_METHOD(bool, release_effect    , (ALLEGRO_HAPTIC_EFFECT_ID *));
+   
 } ALLEGRO_HAPTIC_DRIVER;
 
 
@@ -56,24 +64,22 @@ extern ALLEGRO_HAPTIC_DRIVER * _al_haptic_driver;
 
 #define _AL_HAPTIC_INFO_NAME_MAX            256
    
-/* Can playback at most 32 haptic effects at the same time. */   
+/* Can upload at most 32 haptic effects at the same time. */   
 #define _AL_HAPTIC_EFFECT_PLAYBACK_MAX      32    
 
    
-/* information about an entire haptic */
-typedef struct _AL_HAPTIC_INFO
-{
-  int  id;
-  int  flags; 
-  int  num_axes;
-  char name[_AL_HAPTIC_INFO_NAME_MAX];
-} _AL_HAPTIC_INFO;
-
+#define _AL_HAPTIC_FROM_JOYSTICK        1
+#define _AL_HAPTIC_FROM_MOUSE           2
+#define _AL_HAPTIC_FROM_KEYBOARD        3
+#define _AL_HAPTIC_FROM_DISPLAY         4
+#define _AL_HAPTIC_FROM_TOUCH_INPUT     5
 
 
 struct ALLEGRO_HAPTIC
 {
-   _AL_HAPTIC_INFO info;
+  int    from;
+  void * device;
+  double gain;
 };
 
 void _al_generate_haptic_event(ALLEGRO_EVENT *event);
